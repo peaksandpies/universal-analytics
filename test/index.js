cdcb44d45c28eea767f16c14d2671b05631bc10f
kAlmAcetA <kalmaceta@2147483647.net>
2013-12-30 02:06:22 +0100

    Fixes Issue #7

--- test/index.js
+++ test/index.js
@@ -62,7 +62,7 @@ describe("ua", function () {
 
 		var visitor = ua(options);
 
-		should.not.exist(visitor.tid)
+		visitor.should.not.have.properties('tid')
 		utils.isUuid(visitor.cid).should.equal(true, "A valid random UUID should have been generated")
 		visitor.options.should.eql(options)
 	});
